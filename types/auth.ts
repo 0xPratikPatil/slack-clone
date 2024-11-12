86674ff0c27ce541b3ca287566a1e4ab87eba7c2
Pratik Patil <dev.pratikpatil@proton.me>
2024-11-12 19:54:53 +0530

    Final DB And Types

---
+++ types/auth.ts
@@ -0,0 +1,7 @@
+import type { auth } from "../lib/auth/auth";
+import { authClient } from "../lib/auth/auth-client";
+
+
+export type Session = typeof auth.$Infer.Session;
+export type ActiveOrganization = typeof authClient.$Infer.ActiveOrganization;
+export type Invitation = typeof authClient.$Infer.Invitation;
