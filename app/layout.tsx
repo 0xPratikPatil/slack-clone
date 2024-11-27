48a1d57caa995b160faeae09f3b89591cfcc8571
Pratik Patil <dev.pratikpatil@proton.me>
2024-11-27 23:06:19 +0530

    App

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,35 @@
-
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Space_Grotesk } from "next/font/google"
+import { Space_Grotesk } from "next/font/google";
 import { QueryProviders } from "@/components/query-provider";
 import { cn } from "@/lib/utils";
+import { JotaiProvider } from "@/components/jotai-provider";
+import { ModalProvider } from "@/components/modal-provider";
 
-
-const sg = Space_Grotesk({ subsets: ["latin"] })
+const sg = Space_Grotesk({ subsets: ["latin"] });
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<head>
-				<link rel="icon" href="/favicon/favicon.ico" sizes="any" />
-			</head>
-			<body className={cn(sg.className, "antialiased min-h-screen")}
-			>
-				<ThemeProvider attribute="class" defaultTheme="dark">
-					<QueryProviders>
-						{children}
-					</QueryProviders>
-					<Toaster richColors />
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+      </head>
+      <body className={cn(sg.className, "antialiased min-h-screen")}>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <QueryProviders>
+            <JotaiProvider>
+              <ModalProvider />
+              {children}
+              <Toaster richColors />
+            </JotaiProvider>
+          </QueryProviders>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
