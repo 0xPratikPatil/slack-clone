41c664bf3d86ad754f22f4bdb2e1a2bf8c89ea77
Pratik Patil <dev.pratikpatil@proton.me>
2024-11-29 23:59:46 +0530

    Work Components

--- app/(root)/workspace/[workspaceId]/page.tsx
+++ app/(root)/workspace/[workspaceId]/page.tsx
@@ -1,54 +1,68 @@
-// 'use client';
+"use client";
 
-// import { Loader, TriangleAlert } from 'lucide-react';
-// import { useRouter } from 'next/navigation';
-// import { useEffect, useMemo } from 'react';
+import { Loader, TriangleAlert } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useMemo } from "react";
 
-// import { useGetChannels } from '@/features/channels/api/use-get-channels';
-// import { useCreateChannelModal } from '@/features/channels/store/use-create-channel-modal';
-// import { useGetWorkspaceInfo } from '@/features/workspaces/api/use-get-workspace-info';
-// import { useWorkspaceId } from '@/hooks/use-workspace-id';
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
+import { useGetWorkspaceInfo } from "@/features/workspaces/api/use-get-workspace-info";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
-// const WorkspaceIdPage = () => {
-//   const router = useRouter();
-//   const workspaceId = useWorkspaceId();
-//   const [open, setOpen] = useCreateChannelModal();
+const WorkspaceIdPage = () => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+  const [open, setOpen] = useCreateChannelModal();
 
-//   const { data: workspace, isLoading: workspaceLoading } = useGetWorkspaceInfo({ id: workspaceId });
-//   const { data: channels, isLoading: channelsLoading } = useGetChannels({ workspaceId });
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspaceInfo({
+    workspaceId,
+  });
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({
+    workspaceId,
+  });
 
-//   const channelId = useMemo(() => channels?.[0]?._id, [channels]);
+  const channelId = useMemo(() => channels?.data?.[0]?.id, [channels]);
 
-//   useEffect(() => {
-//     if (workspaceLoading || channelsLoading || !workspace) return;
+  useEffect(() => {
+    if (workspaceLoading || channelsLoading || !workspace) return;
 
-//     if (channelId) router.replace(`/workspace/${workspaceId}/channel/${channelId}`);
-//     else if (!open && workspace.role === 'admin') setOpen(true);
-//   }, [channelId, workspaceLoading, channelsLoading, workspace, open, setOpen, router, workspaceId]);
+    if (channelId)
+      router.replace(`/workspace/${workspaceId}/channel/${channelId}`);
+    else if (!open && workspace?.data?.role === "admin") setOpen(true);
+  }, [
+    channelId,
+    workspaceLoading,
+    channelsLoading,
+    workspace,
+    open,
+    setOpen,
+    router,
+    workspaceId,
+  ]);
 
-//   if (workspaceLoading || channelsLoading) {
-//     return (
-//       <div className="flex h-full flex-1 flex-col items-center justify-center gap-2 bg-[#5E2C5F]/95 text-white">
-//         <Loader className="size-5 animate-spin" />
-//       </div>
-//     );
-//   }
+  if (workspaceLoading || channelsLoading) {
+    return (
+      <div className="flex h-full flex-1 flex-col items-center justify-center gap-2 bg-[#5E2C5F]/95 text-white">
+        <Loader className="size-5 animate-spin" />
+      </div>
+    );
+  }
 
-//   if (!workspaceId || !workspace) {
-//     return (
-//       <div className="flex h-full flex-1 flex-col items-center justify-center gap-2 bg-[#5E2C5F]/95 text-white">
-//         <TriangleAlert className="size-5" />
-//         <span className="text-sm">Workspace not found.</span>
-//       </div>
-//     );
-//   }
+  if (!workspaceId || !workspace) {
+    return (
+      <div className="flex h-full flex-1 flex-col items-center justify-center gap-2 bg-[#5E2C5F]/95 text-white">
+        <TriangleAlert className="size-5" />
+        <span className="text-sm">Workspace not found.</span>
+      </div>
+    );
+  }
 
-//   return (
-//     <div className="flex h-full flex-1 flex-col items-center justify-center gap-2 bg-[#5E2C5F]/95 text-white">
-//       <TriangleAlert className="size-5" />
-//       <span className="text-sm">No Channel(s) found.</span>
-//     </div>
-//   );
-// };
+  return (
+    <div className="flex h-full flex-1 flex-col items-center justify-center gap-2 bg-[#5E2C5F]/95 text-white">
+      <TriangleAlert className="size-5" />
+      <span className="text-sm">No Channel(s) found.</span>
+    </div>
+  );
+};
 
-// export default WorkspaceIdPage;
+export default WorkspaceIdPage;
