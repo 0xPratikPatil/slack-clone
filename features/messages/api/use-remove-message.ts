23023417cf27390d1632a550bf3d53f88c878f05
Pratik Patil <dev.pratikpatil@proton.me>
2024-12-21 23:19:11 +0530

    Meassages

---
+++ features/messages/api/use-remove-message.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { InferRequestType, InferResponseType } from 'hono';
+import { toast } from 'sonner';
+
+import { client } from '@/lib/rpc';
+
+type ResponseType = InferResponseType<(typeof client.api.messages)['remove-message']['$post']>;
+type RequestType = InferRequestType<(typeof client.api.messages)['remove-message']['$post']>;
+
+export const useRemoveMessage = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ json }) => {
+      const response = await client.api.messages['remove-message']['$post']({ json });
+      return await response.json();
+    },
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({ queryKey: ['messages'] });
+      toast.success(response.message);
+    },
+    onError: (error: Error) => {
+      toast.error(error.message || 'Something went wrong while removing message');
+    },
+  });
+
+  return mutation;
+};
